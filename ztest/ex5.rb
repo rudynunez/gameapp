22401604fb2a1734959837376d2fc2da6b5a2c1b
Rudy Nunez <rudy.nunez@gmail.com>
2017-11-05 01:48:55 +0000

    Messed around unsuccessfully with pages controller to try to get ruby output to display on home page. Completed lessons 1 - 9 in Learn Ruby the Hard Way (file folder: ztest)

---
+++ ztest/ex5.rb
@@ -0,0 +1,16 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "let's talk about #{name}."
+puts "He's #{height / 12} feet tall."
+puts "he's #{weight * 2.2} kilos heavy."
+puts "Actually, that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height / 12} and #{weight * 2.2} I get #{age + height + weight}."
---
+++ ztest/ex9.rb
@@ -0,0 +1,18 @@
+# Here's some strange new stuff, remember type it exactly.
+
+# when variable called, prints just how it reads
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# when variable called, prints each month on a new line
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+# fancy function that displays what you type inside of it just as it appears within the braces
+puts %q{
+  There's something going on here.
+  With this weird quote
+  We'll be able to type as much as we like.
+  Even 4 lines if we want, or 5, or 6.
+}
